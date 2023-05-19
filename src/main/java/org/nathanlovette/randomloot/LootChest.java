c5ba6fec4a8b3ea065918bed93d0d01f7793f74a
Nathan Lovette <lovettenathan@gmail.com>
2023-05-19 14:25:16 -0500

    define custom classes

--- src/main/java/org/nathanlovette/randomloot/LootChest.java
+++ src/main/java/org/nathanlovette/randomloot/LootChest.java
@@ -1,4 +1,11 @@
 package org.nathanlovette.randomloot;
 
+import java.util.ArrayList;
+
 public class LootChest {
+    public int x;
+    public int y;
+    public int z;
+
+    public ArrayList<LootItem> items = new ArrayList<>();
 }
--- src/main/java/org/nathanlovette/randomloot/LootItem.java
+++ src/main/java/org/nathanlovette/randomloot/LootItem.java
@@ -1,4 +1,9 @@
 package org.nathanlovette.randomloot;
 
+import org.bukkit.Material;
+
 public class LootItem {
+    public Material material;
+    public int minimumAmount;
+    public int maximumAmount;
 }
